8fb4a10f31fa1b3c040d51403049272666631934
Manshuk Nagymetkyzy <nagymetqyzy@gmail.com>
2022-12-27 15:35:53 +0600

    Manshuk

---
+++ frontend/transfers.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/Anel & Manshuk/transfers.css">
+    <title>My Bank</title>
+</head>
+<body>
+    <div id="head">
+        <img src="./image/back-button.png" id="back" align="left">
+        <img src="./image/Logo.png" width="50">
+        <span id="title">Переводы</span>
+        <div id="second-header">
+            <div id="box">
+                <span id="nav">МОИ ПЕРЕВОДЫ</span>
+                <span id="aside">ИСТОРИЯ</span>
+            </div>
+        </div>
+    </div>
+    <div id="main">
+        <div id="types">
+            <div class="type">
+                <img class="photos" src="./image/change.png">
+                <div class="just">
+                <div class="texts">
+                    <span class="text1">Между своими счетами</span>
+                    <span class="text2"></span>
+                </div>
+                <img class="photo2" src="./image/right.png">
+                </div>
+            </div>
+            <div class="type">
+                <img class="photos" src="./image/reduser.png">
+                <div class="just">
+                <div class="texts">
+                    <span class="text1">Клиенту Kaspi</span><br>
+                    <span class="text2">На карту Kaspi Gold</span>
+                </div>
+                <img class="photo2" src="./image/right.png">
+                </div>
+            </div>
+            <div class="type">
+                <img class="photos" src="./image/cards.png">
+                <div class="just">
+                <div class="texts">
+                    <span class="text1">Карта другого банка</span><br>
+                    <span class="text2">С карты на карту</span>
+                </div>
+                <img class="photo2" src="./image/right.png">
+                </div>
+            </div>
+            <div class="type">
+                <img class="photos" src="./image/world.png">
+                <div class="just">
+                <div class="texts">
+                    <span class="text1">Международные переводы</span><br>
+                    <span class="text2">По номеру карты или телефона</span>
+                </div>
+                <img class="photo2" src="./image/right.png">
+                </div>
+            </div>
+            <div class="type">
+                <img class="photos" src="./image/qrcode.png">
+                <div class="just">
+                <div class="texts">
+                    <span class="text1">Kaspi QR</span><br>
+                    <span class="text2">Сканируйте и платите</span>
+                </div>
+                <img class="photo2" src="./image/right.png">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="footer">
+        <div class="categories">
+            <img src="./image/mainpage.png" width="32px">
+            <span>Главная</span>
+        </div>
+        <div class="categories">
+            <img src="./image/qr.png" width="32px">
+            <span>Kaspi QR</span>
+        </div>
+        <div class="categories">
+            <img src="./image/message.png" width="32px">
+            <span>Сообщения</span>
+        </div>
+        <div class="categories">
+            <img src="./image/services.png" width="32px">
+            <span>Сервисы</span>
+        </div>
+        
+    </div>
+</body>
+</html>
